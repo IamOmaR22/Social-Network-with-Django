7d8c6e56f76b17743077b30d4e2d9935aea8058c
IamOmaR22 <iamomar022@gmail.com>
2021-04-12 22:57:09 +0600

    Add friends and Remove friends

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,8 @@ from .views import (
     invite_profiles_list_view,
     # profiles_list_view, 
     ProfileListView,
+    send_invitations,
+    remove_from_friends,
 )
 
 app_name = 'profiles'
@@ -15,4 +17,6 @@ urlpatterns = [
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
     # path('all-profiles/', profiles_list_view, name='all-profiles-view'),
     path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'),
+    path('send-invite/', send_invitations, name='send-invite'),
+    path('remove-friend/', remove_from_friends, name='remove-friend'),
 ]
