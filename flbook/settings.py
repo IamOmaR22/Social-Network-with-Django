79724d5628b2050368c0831778a95381d4654cf2
IamOmaR22 <iamomar022@gmail.com>
2021-07-07 13:05:09 +0600

    Navbar

--- flbook/settings.py
+++ flbook/settings.py
@@ -37,12 +37,33 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'profiles',
     'posts',
+
+    # django all-auth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
-LOGIN_URL = '/admin'
+SITE_ID = 1
+
+# LOGIN_URL = '/admin'
+LOGIN_REDIRECT_URL = '/posts'
+
+# Login with email instead of username
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_UNIQUE = True
+# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # When we go live
+
+# For SignUp [Without this showing ConnectionRefusedError]
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # For development while go live
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,6 +93,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'flbook.wsgi.application'
