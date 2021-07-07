00458ca9068bcef584a1b812cbdab283ffdc4d1b
IamOmaR22 <iamomar022@gmail.com>
2021-07-07 10:48:16 +0600

    Context Processors

--- flbook/settings.py
+++ flbook/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'profiles.context_processors.profile_pic',   # From profiles/context_processors.py
+                'profiles.context_processors.invitations_received_no',   # From profiles/context_processors.py
             ],
         },
     },
